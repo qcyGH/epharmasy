5a5d37ed22ba1630a2028b2040e12a4b0e6ca970
Max <12maks32@gmail.com>
2023-03-27 14:58:46 +0200

    Added error message to index page
    Error message about connecting to a database

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,6 +10,7 @@ import { fetchItems } from '@/store/shopSlice'
 export default function Home(props) {
   const dispatch = useDispatch()
   const [isLoading, setLoading] = useState(true)
+  const [status, setStatus] = useState(0)
 
 
   useEffect(() => {
@@ -18,9 +19,13 @@ export default function Home(props) {
       const data = await response.json()
       console.log(data)
 
-      if (data) {
+      if (data.status == '200') {
         dispatch(fetchItems({data}))
         setLoading(false)
+        setStatus(200)
+      } else if (data.status == '500') {
+        setLoading(false)
+        setStatus(500)
       }
     }
 
@@ -31,7 +36,9 @@ export default function Home(props) {
   return (
     <>
       {
-        isLoading ? <Preloader isLoading={isLoading} /> : <List />
+        isLoading
+        ? <Preloader isLoading={isLoading} />
+        : status == '200' ? <List /> : <h1 className='mt-5 text-2xl font-semibold text-center'>Data base error connection</h1>
       }
     </>
   )
