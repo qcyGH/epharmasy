54329f38b56abb2a518ddd4202f919c8f230c8c8
Max <12maks32@gmail.com>
2023-03-27 15:08:31 +0200

    Added API for orders table

---
+++ src/pages/api/orders.jsx
@@ -0,0 +1,21 @@
+import nextConnect from 'next-connect'
+import dbConnection from '@/pages/api/db'
+
+const handler = nextConnect()
+
+handler.get(async (req, res) => {
+  try {
+    const connection = await dbConnection()
+    const [rows] = await connection.query('SELECT * FROM orders')
+    res.status(200).json({
+      data: rows,
+      status: 200
+    })
+    console.info('Database connection successful')
+  } catch (error) {
+    res.status(500).json({ status: 500 })
+    console.info('Database connection error')
+  }
+})
+
+export default handler;
