d4b5b8ed9ce035ee1d008b9fee1615a12093781c
Max <12maks32@gmail.com>
2023-04-01 17:40:32 +0200

    Change db conf

--- src/pages/api/db.jsx
+++ src/pages/api/db.jsx
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 
 const dbConnection = async () => {
   const db = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'su',
-    database: 'epharmasy',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   })
 
   return db;
